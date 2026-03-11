6b9d517ec17140c80e788719866d9f0787468a99
unknown <len.abhijeet@gmail.com>
2026-03-11 09:00:20 +0530

    UC2: Display Hello with Command-Line Argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // UC2: Accept a name from command-line argument and display personalized greeting
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
